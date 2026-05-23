9d04d5bf709e30451d94ddb3e794af112951a01d
cypherpulse <johnbradill67@gmail.com>
2026-05-23 03:14:32 +0300

    Improve guess input handling with error checking in main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -18,14 +18,25 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    //let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    //handling invalid input
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_)=>{
+            println!("Please type a number!");
+            continue;
+        }
+    };
 
     println!("You guessed: {guess}");
 
       match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        }
     }
     }
 }
